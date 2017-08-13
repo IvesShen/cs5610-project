5e14a895b0ee7b994b84ea64a8a1dbd4329a6596
yanchao <870154211@qq.com>
2017-08-13 15:34:41 -0700

    addAdmin, deleteAdmin

--- public/project/views/user/admin-users.controller.client.js
+++ public/project/views/user/admin-users.controller.client.js
@@ -3,15 +3,51 @@
         .module('WebAppMaker')
         .controller('adminUsersController',adminUsersController);
 
-    function adminUsersController(UserService) {
+    function adminUsersController(UserService, $timeout, currentUser) {
         var model = this;
+        model.addAdmin = addAdmin;
+        model.removeAdmin = removeAdmin;
         model.deleteUser = deleteUser;
         function init() {
             findAllUsers()
         }
         init();
-        
-        
+
+        function addAdmin(user) {
+            user.roles = ['USER', 'ADMIN'];
+            UserService
+                .updateUser(user._id, user)
+                .then(function () {
+                    $window.location.reload();
+                }, function () {
+                    model.error = "Fail when add this user as admin.";
+                    $timeout(function () {
+                        model.error = null;
+                    }, 3000);
+                });
+        }
+
+        function removeAdmin(user) {
+            if (user._id === currentUser._id) {
+                model.error = "You cannot change roles of yourself!";
+                $timeout(function () {
+                    model.error = null;
+                }, 3000);
+            } else {
+                user.roles = ['USER'];
+                UserService
+                    .updateUser(user._id, user)
+                    .then(function () {
+                        $window.location.reload();
+                    }, function () {
+                        model.error = "Fail when remove this user as admin.";
+                        $timeout(function () {
+                            model.error = null;
+                        }, 3000);
+                    });
+            }
+        }
+
         function deleteUser(user) {
             UserService
                 .deleteUser(user._id)
